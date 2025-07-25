79cca728d3eaa674188262e70130a3cccd88527e
EkaterinaMamedov <temp44x@gmail.com>
2025-07-25 10:59:40 +0300

    home work 25.07.25

--- src/test/java/HomeWork.java
+++ src/test/java/HomeWork.java
@@ -22,32 +22,54 @@ public class HomeWork {
     public void cssSelectors() {
         //by tagName
         WebElement form = wd.findElement(By.tagName("form"));
+        System.out.println("form role-->" + form.getAriaRole());
         WebElement button = wd.findElement(By.tagName("button"));
+        System.out.println("button name-->" + button.getAttribute("name"));
         WebElement input = wd.findElement(By.tagName("input"));
-        WebElement a = wd.findElement(By.tagName("a"));
-        WebElement phonebook = wd.findElement(By.tagName("h1"));
+        System.out.println("input name-->" + input.getAttribute("name"));
 
-        List<WebElement> listA = wd.findElements(By.tagName("a"));
         List<WebElement> listInput = wd.findElements(By.tagName("input"));
+       for(WebElement i : listInput){
+           System.out.println("input name-->" + i.getAttribute("name"));
+       }
         List<WebElement> listButton = wd.findElements(By.tagName("button"));
+        for(WebElement b : listButton){
+            System.out.println("button name-->" + b.getAttribute("name"));
+        }
 
         //by name
         WebElement inputEmail = wd.findElement(By.name("email"));
+        System.out.println("placeholder email-->" + inputEmail.getAttribute("placeholder"));
         WebElement inputPassword = wd.findElement(By.name("password"));
+        System.out.println("placeholder password-->" + inputPassword.getAttribute("placeholder"));
         WebElement buttonLog = wd.findElement(By.name("login"));
+        System.out.println("buttonLog name-->" + buttonLog.getAttribute("name"));
         WebElement buttonReg = wd.findElement(By.name("registration"));
+        System.out.println("buttonReg name-->" + buttonReg.getAttribute("name"));
+
         //by className
         WebElement classNameForm = wd.findElement(By.className("login_login__3EHKB"));
+        System.out.println("classNameForm-->" + classNameForm.getAttribute("class"));
         WebElement classNameHeaderAll = wd.findElement(By.className("container"));
+        System.out.println("classNameHeaderAll-->" + classNameHeaderAll.getAttribute("class"));
         WebElement classNameHeader = wd.findElement(By.className("navbar-component_nav__1X_4m"));
+        System.out.println("classNameHeader-->" + classNameHeader.getAttribute("class"));
+
         //by id
         WebElement id = wd.findElement(By.id("root"));
+        System.out.println("id class name-->" + id.getAttribute("class"));
+
         //by text
         // WebElement text = wd.findElement(By.linkText("PHONEBOOK"));
 //by cssSelector
         WebElement home = wd.findElement(By.cssSelector("a[href='/home']"));
+        System.out.println("home -->" + home.getAttribute("href"));
         WebElement about = wd.findElement(By.cssSelector("a[href='/about']"));
+        System.out.println("about -->" + about.getAttribute("href"));
         WebElement loginHeder = wd.findElement(By.cssSelector("a[href='/login']"));
+        System.out.println("loginHeder -->" + loginHeder.getAttribute("href"));
+        WebElement phonebookCss = wd.findElement(By.cssSelector("h1"));
+        System.out.println("phonebookCss -->" + phonebookCss.getAccessibleName());
 
 
     }
